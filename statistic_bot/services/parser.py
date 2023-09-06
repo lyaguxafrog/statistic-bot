f9320c11ceb3873852c704592e9978d19f903778
adrian <maccree.a@yandex.ru>
2023-09-06 05:57:28 +0300

    paars

--- statistic_bot/services/parser.py
+++ statistic_bot/services/parser.py
@@ -1,114 +1,65 @@
 # -*- coding: utf-8 -*-
 # /statistic_bot/services/parser.py
 
-import os 
-from dotenv import load_dotenv, find_dotenv
+import os
 import json
-
-from telethon.sync import TelegramClient
-from telethon import connection
-
-# для корректного переноса времени сообщений в json
-from datetime import date, datetime
-
-# классы для работы с каналами
-from telethon.tl.functions.channels import GetParticipantsRequest
-from telethon.tl.types import ChannelParticipantsSearch
-
-# класс для работы с сообщениями
+from datetime import datetime
 from telethon.tl.functions.messages import GetHistoryRequest
+from telegram_client import start_client, stop_client
 
-# Считываем учетные данные
-load_dotenv(find_dotenv())
+async def dump_all_messages(channel, total_count_limit=0):
+    """Записывает json-файл с информацией о всех сообщениях канала/чата"""
+    offset_msg = 0    # номер записи, с которой начинается считывание
+    limit_msg = 99999   # максимальное число записей, передаваемых за один раз
 
-# Присваиваем значения внутренним переменным
-api_id   = os.getenv('API_ID')
-api_hash = os.getenv('API_HASH')
-# username = os.getenv('API')
+    all_messages = []   # список всех сообщений
+    total_messages = 0
 
+    class DateTimeEncoder(json.JSONEncoder):
+        '''Класс для сериализации записи дат в JSON'''
+        def default(self, o):
+            if isinstance(o, datetime):
+                return o.isoformat()
+            if isinstance(o, bytes):
+                return list(o)
+            return json.JSONEncoder.default(self, o)
 
-client = TelegramClient(api_id=api_id, api_hash=api_hash, session="lyaguxa_forg")
+    while True:
+        history = await client(GetHistoryRequest(
+            peer=channel,
+            offset_id=offset_msg,
+            offset_date=None, add_offset=0,
+            limit=limit_msg, max_id=0, min_id=0,
+            hash=0))
+        if not history.messages:
+            break
+        messages = history.messages
+        for message in messages:
+            all_messages.append(message.to_dict())
+        offset_msg = messages[len(messages) - 1].id
+        total_messages = len(all_messages)
+        if total_count_limit != 0 and total_messages >= total_count_limit:
+            break
 
-client.start()
-
-
-async def dump_all_participants(channel):
-	"""Записывает json-файл с информацией о всех участниках канала/чата"""
-	offset_user = 0    # номер участника, с которого начинается считывание
-	limit_user = 100   # максимальное число записей, передаваемых за один раз
-
-	all_participants = []   # список всех участников канала
-	filter_user = ChannelParticipantsSearch('')
-
-	while True:
-		participants = await client(GetParticipantsRequest(channel,
-			filter_user, offset_user, limit_user, hash=0))
-		if not participants.users:
-			break
-		all_participants.extend(participants.users)
-		offset_user += len(participants.users)
-
-	all_users_details = []   # список словарей с интересующими параметрами участников канала
-
-	for participant in all_participants:
-		all_users_details.append({"id": participant.id,
-			"first_name": participant.first_name,
-			"last_name": participant.last_name,
-			"user": participant.username,
-			"phone": participant.phone,
-			"is_bot": participant.bot})
-
-	with open('channel_users.json', 'w', encoding='utf8') as outfile:
-		json.dump(all_users_details, outfile, ensure_ascii=False)
+    with open('channel_messages.json', 'w', encoding='utf8') as outfile:
+        json.dump(all_messages, outfile, ensure_ascii=False, cls=DateTimeEncoder)
 
 
-async def dump_all_messages(channel):
-	"""Записывает json-файл с информацией о всех сообщениях канала/чата"""
-	offset_msg = 0    # номер записи, с которой начинается считывание
-	limit_msg = 100   # максимальное число записей, передаваемых за один раз
-
-	all_messages = []   # список всех сообщений
-	total_messages = 0
-	total_count_limit = 0  # поменяйте это значение, если вам нужны не все сообщения
-
-	class DateTimeEncoder(json.JSONEncoder):
-		'''Класс для сериализации записи дат в JSON'''
-		def default(self, o):
-			if isinstance(o, datetime):
-				return o.isoformat()
-			if isinstance(o, bytes):
-				return list(o)
-			return json.JSONEncoder.default(self, o)
-
-	while True:
-		history = await client(GetHistoryRequest(
-			peer=channel,
-			offset_id=offset_msg,
-			offset_date=None, add_offset=0,
-			limit=limit_msg, max_id=0, min_id=0,
-			hash=0))
-		if not history.messages:
-			break
-		messages = history.messages
-		for message in messages:
-			all_messages.append(message.to_dict())
-		offset_msg = messages[len(messages) - 1].id
-		total_messages = len(all_messages)
-		if total_count_limit != 0 and total_messages >= total_count_limit:
-			break
-
-	with open('channel_messages.json', 'w', encoding='utf8') as outfile:
-		 json.dump(all_messages, outfile, ensure_ascii=False, cls=DateTimeEncoder)
+async def main(url: str, total_count_limit=0):
+    await start_client()
+    channel = await client.get_entity(url)
+    await dump_all_messages(channel, total_count_limit)
+    await stop_client()
 
 
-async def main():
-	url = input("Введите ссылку на канал или чат: ")
-	channel = await client.get_entity(url)
-	await dump_all_participants(channel)
-	await dump_all_messages(channel)
+if __name__ == "__main__":
+    import argparse
 
+    parser = argparse.ArgumentParser(description="Dump messages from a Telegram channel.")
+    parser.add_argument("channel_url", type=str, help="URL of the channel")
+    parser.add_argument("--message_limit", type=int, default=0, help="Total message limit (0 for all)")
 
-with client:
-	client.loop.run_until_complete(main())
-
-
+    args = parser.parse_args()
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(args.channel_url, args.message_limit))
